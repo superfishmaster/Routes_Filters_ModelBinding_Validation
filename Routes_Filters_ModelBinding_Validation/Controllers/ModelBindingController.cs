355b5097418620ffabf805a741ddcb0a5d25867e
superfishmaster <48781827+superfishmaster@users.noreply.github.com>
2020-04-10 12:25:29 -0400

    new

--- Routes_Filters_ModelBinding_Validation/Controllers/ModelBindingController.cs
+++ Routes_Filters_ModelBinding_Validation/Controllers/ModelBindingController.cs
@@ -7,35 +7,49 @@ using Routes_Filters_ModelBinding_Validation.Models;
 
 namespace Routes_Filters_ModelBinding_Validation.Controllers
 {
-  public class ModelBindingController : Controller
-  {
-    private IRepository repository;
-
-    public ModelBindingController(IRepository repo)
+    public class ModelBindingController : Controller
     {
-      repository = repo;
+        private IRepository repository;
+
+        public ModelBindingController(IRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index(int id)
+        {
+            return View(repository[id]);
+        }
+        //public ViewResult Index(int id) => View(repository[id]);
+        public ViewResult IndexWithAddress(int id) => View(repository[id]);
+
+        public ViewResult IndexNullSafe(int id) => View(repository[id] ?? repository.People.First());
+
+        public ViewResult Create() => View(new Person());
+
+        [HttpPost]
+        public ViewResult Create(Person model) => View("IndexNullSafe", model);
+
+        public ViewResult CreateWithAddress() => View(new Person());
+
+        [HttpPost]
+        public ViewResult CreateWithAddress(Person model) => View("IndexWithAddress", model);
+
+        public ViewResult NamesArray(string[] names) => View(names ?? new string[0]);
+
+        public ViewResult NamesList(IList<string> names) => View(names ?? new List<string>());
+
+        public ViewResult Address(IList<AddressSummary> addresses) => View(addresses ?? new List<AddressSummary>());
+        public ViewResult CreatePhoneNumber()
+        {
+            Phone PhoneNumber = new Phone();
+            return View(PhoneNumber);
+
+        }
+        [HttpPost]
+        public ViewResult CreatePhoneNumber(Phone PhoneNumber)
+        {
+            return View(PhoneNumber);
+        }
     }
-
-    public ViewResult Index(int id) => View(repository[id]);
-
-    public ViewResult IndexWithAddress(int id) => View(repository[id]);
-
-    public ViewResult IndexNullSafe(int id) => View(repository[id] ?? repository.People.First());
-
-    public ViewResult Create() => View(new Person());
-
-    [HttpPost]
-    public ViewResult Create(Person model) => View("IndexNullSafe", model);
-
-    public ViewResult CreateWithAddress() => View(new Person());
-
-    [HttpPost]
-    public ViewResult CreateWithAddress(Person model) => View("IndexWithAddress", model);
-
-    public ViewResult NamesArray(string[] names) => View(names ?? new string[0]);
-
-    public ViewResult NamesList(IList<string> names) => View(names ?? new List<string>());
-
-    public ViewResult Address(IList<AddressSummary> addresses) => View(addresses ?? new List<AddressSummary>());
-  }
 }
---
+++ Routes_Filters_ModelBinding_Validation/Models/phone.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Routes_Filters_ModelBinding_Validation.Models
+{
+    public class Phone
+    {
+        public int PhoneId { get; set; }
+        public string PhoneNember { get; set; }
+        
+    }
+}
